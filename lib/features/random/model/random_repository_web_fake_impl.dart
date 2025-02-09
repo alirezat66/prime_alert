a4de6b59677b63f8ebd2c1c81e692cfacee83dae
Reza Taghizadeh <alirezataghizadeh@gmail.com>
2025-02-09 20:40:19 +0100

    feat: enhance random number generation with debug output and adjust range

--- lib/features/random/model/random_repository_web_fake_impl.dart
+++ lib/features/random/model/random_repository_web_fake_impl.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:prime_alert/features/random/model/data/timed_number.dart';
 import 'package:prime_alert/features/random/model/random_repository.dart';
+
 // For solving cors problem on api
 class RandomRepositoryWebFakeImpl implements RandomRepository {
   @override
@@ -9,7 +11,8 @@ class RandomRepositoryWebFakeImpl implements RandomRepository {
     await Future.delayed(const Duration(milliseconds: 200));
     var random = Random();
     int randomNumber =
-        random.nextInt(200000000); // Generates a random number between 0 and 99
+        random.nextInt(2000); // Generates a random number between 0 and 99
+    debugPrint("Random number generated: $randomNumber");
     return TimedNumber(number: randomNumber, responseDate: DateTime.now());
   }
 }
